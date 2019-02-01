0841c4159fcf2ea957a5fc0707ab885dfb946c28
Sion Vives Bernat <svives@soffid.com>
2019-02-01 12:04:22 +0100

    New tags, improvements and some issues
    - New property *NotFound
    - New property *Avoid
    - Issue in getRoleList
    - Stop process if there are no roles
    - Change RuntimeException
    - Issue in the URL encode when there are no parameeters
    - Issue in the generation of the format xml data

--- src/main/java/com/soffid/iam/sync/agent/json/InvocationMethod.java
+++ src/main/java/com/soffid/iam/sync/agent/json/InvocationMethod.java
@@ -1,6 +1,5 @@
 package com.soffid.iam.sync.agent.json;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class InvocationMethod
@@ -13,6 +12,8 @@ public class InvocationMethod
 	public String check;
 	public String next;
 	public String template;
+	public String notFound;
+	public String avoid;
 	public String[] parameters;
 	public List<String[]> headers;
 }
